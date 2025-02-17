5776c90caeba1196062a331e4182a93bb1baaf93
lighthoof <andrew.kuligin@gmail.com>
2025-02-17 18:38:56 +0200

    implemented framerate limit

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import pygame
 from constants import *
 
-def main(): 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+def main():
     pygame.init()
     is_running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill(000000)
-
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick()/1000
 
 if __name__ == "__main__":
     main()
